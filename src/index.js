5ddcae5f5c3152b552309f0d265ecf9be4f9493a
zikou058 <rajawizakaria546@gmail.com>
2026-02-07 18:41:54 +0100

    lern how anrufe fur function in anderen file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
+import HalloWorld from './.halloWorld/HalloWorld';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HalloWorld />
   </React.StrictMode>
 );
 
