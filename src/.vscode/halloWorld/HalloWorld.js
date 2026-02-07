ce63e4c51e7b90c65366918507a344e360e9810f
zikou058 <rajawizakaria546@gmail.com>
2026-02-07 19:10:13 +0100

    function component werdest dynamic

--- src/.vscode/halloWorld/HalloWorld.js
+++ src/.vscode/halloWorld/HalloWorld.js
@@ -1,17 +1,18 @@
 // method 1 to creat a component with function 
-// export default function halloWorld() {
-//     return (
-//         <h1>Hello World!</h1>
-//     );
-// }
+export default function halloWorld(props) {
+    console.log(props);
+    return (
+        <h1>Hello  {props.lastName}</h1>
+    );
+}
 
 // Anther method 2 to creat a component 
-import {Component} from "react";
+// import {Component} from "react";
 
-export default class HalloWorld extends Component {
-    render() {
-        return (
-            <h1>Hello World!</h1>
-        );
-    }
-}
+// export default class HalloWorld extends Component {
+//     render() {
+//         return (
+//             <h1>Hello World!</h1>
+//         );
+//     }
+// }
--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 //import App from './App';
-import HalloWorld from './.halloWorld/HalloWorld';
+import HalloWorld from './.vscode/halloWorld/HalloWorld';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HalloWorld />
+    <HalloWorld lastName='Mennis' /> 
   </React.StrictMode>
 );
 
