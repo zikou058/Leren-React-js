fcaa5a6fa8f03784cfc7ec450f1773a38eb03af8
zikou058 <rajawizakaria546@gmail.com>
2026-02-08 15:51:27 +0100

    change path

---
+++ src/halloWorld/HalloWorld.js
@@ -0,0 +1,18 @@
+// method 1 to creat a component with function 
+// export default function halloWorld({lastName}) {
+//     //console.log(props);
+//     return (
+//         <h1>Hello {lastName}</h1>
+//     );
+// }
+
+// Anther method 2 to creat a component 
+import {Component} from "react";
+
+export default class HalloWorld extends Component {
+    render() {
+        return ( 
+            <h1>Hello {this.props.lastName} </h1>
+        );
+    }
+}
