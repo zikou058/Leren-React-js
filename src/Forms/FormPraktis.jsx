1f29d18edfaf0ce892e2c121e472fa3a8cf6b2aa
zikou058 <rajawizakaria546@gmail.com>
2026-03-12 23:31:26 +0100

    update

--- src/Forms/FormPraktis.jsx
+++ src/Forms/FormPraktis.jsx
@@ -1,13 +1,18 @@
 import { useRef} from 'react'
+import { useEffect } from 'react'
 export default function FormPraktis () {
     const inputRef = useRef();
 
     const handleClick = () => {
       alert(inputRef.current.value)
     }
+    useEffect(() => {
+      name: inputRef.current.fucus()
+      
+    }, []);
     return (
         <div>
-            <input ref={inputRef} type="text"/>
+            <input id='name' ref={inputRef} type="text"/>
             <button onClick={handleClick}>Show Value</button>
         </div>
     )
