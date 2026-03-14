719ee9c16c35287bddff90c81cc4432a53859fd6
zikou058 <rajawizakaria546@gmail.com>
2026-03-14 22:09:46 +0100

    exercice for form with hooks

--- src/Forms/FormPraktis.jsx
+++ src/Forms/FormPraktis.jsx
@@ -1,19 +1,40 @@
-import { useRef} from 'react'
-import { useEffect } from 'react'
+import  {useRef} from 'react'
+import { useState } from 'react'
+import  {useEffect}  from 'react'
 export default function FormPraktis () {
-    const inputRef = useRef();
+    const inputNameRef = useRef();
+    const inputEmailRef = useRef();
+    const inputPaswordRef = useRef();
+    const inputUsernameRef = useRef();
+    const [valeur, setValeur] = useState({
+        name : ''
+    });
 
     const handleClick = () => {
-      alert(inputRef.current.value)
+      setValeur({
+        name : inputNameRef.current.value,
+        email : inputEmailRef.current.value
+      })
+      console.log("Email:",inputEmailRef.current.value)
+      console.log("Name:",inputNameRef.current.value)
     }
     useEffect(() => {
-      name: inputRef.current.fucus()
-      
+      inputUsernameRef.current.focus(),
+      inputPaswordRef.current.focus(),
     }, []);
     return (
         <div>
-            <input id='name' ref={inputRef} type="text"/>
-            <button onClick={handleClick}>Show Value</button>
+            <h2>objet: </h2>
+            {JSON.stringify(valeur)}
+            <div>
+                <h5>Name :</h5><input id='name' ref={inputNameRef} type="text"/>
+                <h5>Email: </h5><input type="email" ref={inputEmailRef}/>
+                <h5>Password: </h5><input type="password" ref={inputPaswordRef}/>
+                
+            </div>
+            <div>
+                <button onClick={handleClick}>Show Value</button>
+            </div>
         </div>
     )
 }
